1ec8467d0c2fa932ecd4b2e367da0c3c424915b5
Jacquet L <jacquetleme2021@gmail.com>
2023-03-18 21:42:53 -0500

    first push

---
+++ clickup/src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-header">
+        <div className="container-header-logo">
+          <div className="container-header-logo-svg">
+            <span className="icone-svg">
+            <img src = "happy.svg" alt="My Happy SVG"/>
+            </span>
+          </div>
+          
+          <div className="container-header-logo-text">
+            <h1 className="ClickUp"></h1>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
