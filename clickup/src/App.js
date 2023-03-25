ad60b3a2d00b3032b8d5bc3bf5df3473bd1d3112
Jacquet L <jacquetleme2021@gmail.com>
2023-03-25 17:55:48 -0500

    modificação no head

--- clickup/src/App.js
+++ clickup/src/App.js
@@ -1,21 +1,32 @@
+import jpIMG from "./images/logo-clickup_color.svg"
 
+import './styles.css'
 function App() {
   return (
     <div className="container">
       <div className="container-header">
         <div className="container-header-logo">
           <div className="container-header-logo-svg">
-            <span className="icone-svg">
-            <img src = "happy.svg" alt="My Happy SVG"/>
-            </span>
+            <img src={jpIMG} alt=" " />
+
+            <li><a >Product</a></li>
+            <li><a >Solutions</a></li>
+            <li><a >Learn</a> </li>
+            <li><a >Pricing</a></li>
+            <li><a >Enterprise</a></li>
           </div>
-          
-          <div className="container-header-logo-text">
-            <h1 className="ClickUp"></h1>
+
+          <div className="container-header-logo-svg">
+            <li className="texte1">Contact Sales</li>
+            <li className="texte2">Sign Up</li>
+            <li className="texte3">Login Up</li>
           </div>
 
         </div>
       </div>
+
+
+      
     </div>
   );
 }
