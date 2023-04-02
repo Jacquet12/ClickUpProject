2e4a2a77240b962277fef0ed32d5598c38d9efad
Jacquet L <jacquetleme2021@gmail.com>
2023-04-01 20:44:24 -0500

    mudança na parte body

--- clickup/src/App.js
+++ clickup/src/App.js
@@ -1,4 +1,18 @@
 import jpIMG from "./images/logo-clickup_color.svg"
+import g2crowd from "./images/g2crowd.png"
+import capterra from "./images/capterra.png"
+import getapp from "./images/getapp.svg"
+import bookings from "./images/booking_com.svg"
+import ibm from "./images/ibm.svg"
+import logitech from "./images/logitech.svg"
+import fortinet from "./images/fortinet.svg"
+import tibco from "./images/tibco.svg"
+import spotify_logo_with_text from "./images/spotify_logo_with_text.svg"
+import netflix from "./images/netflix.svg"
+import t_mobile_logo from "./images/t-mobile_logo.svg"
+import clickup_v3 from "./images/clickup-v3.svg"
+import CU_3_0_Teaser from "./images/CU_3.0_Teaser.png"
+
 
 import './styles.css'
 function App() {
@@ -37,9 +51,100 @@ function App() {
           <input type="text" placeholder="Digite o seu e-mail de trabalho"></input>
         </div>
 
-
         <div className="container-texto-formulario-botao">
-          <input type="button" value="Iniciar"></input>
+            <div className="container-texto-formulario-botao-card1">
+              <input type="button" value="Iniciar"></input>
+            </div>
+
+            <div className="container-texto-formulario-botao-card2">
+              <p className="container-texto-formulario-botao-texto1" >FREE FOREVER.</p>
+              <p className="container-texto-formulario-botao-texte2">NO CREDIT CARD.</p>
+            </div>
+        </div>
+        
+        <div className="container-header-app-icone">
+
+          <div className="container-header-app-icone1">
+            <img src={g2crowd} alt="g2crowd" />
+          </div>
+
+          <div className="container-header-app-icone2">
+          <img src={capterra} alt="captera" />
+          </div>
+
+          <div className="container-header-app-icone3">
+          <img src={getapp} alt="getapp" />
+          </div>
+
+        </div>
+
+        <div className="conatainer-texte">
+          <h1>JOIN 800,000+ HIGHLY PRODUCTIVE TEAMS</h1>
+        </div>
+
+        <div className="container-redes-sociais">
+          <div container-redes-sociais-logo2>
+            <img src={bookings} alt="bookings" />
+          </div>
+
+          <div className="container-redes-sociais-logo3">
+            <img src={ibm} alt="ibm" />
+          </div>
+
+          <div className="container-redes-sociais-logo4">
+            <img src={logitech} alt="logitech" />
+          </div>
+
+          <div className="container-redes-sociais-logo5">
+            <img src={fortinet} alt="fortinet" />
+          </div>
+
+          <div className="container-redes-sociais-logo6">
+            <img src={tibco} alt="tibco" />
+          </div>
+
+          <div className="container-redes-sociais-logo7">
+            <img src={spotify_logo_with_text} alt="spotify_logo_with_text" />
+          </div>
+
+          <div className="container-redes-sociais-logo8">
+            <img src={t_mobile_logo} alt="t_mobile_logo" />
+          </div>
+
+          <div className="container-redes-sociais-logo9">
+            <img src={netflix} alt="netflix" />
+          </div>
+        </div>
+      
+
+        <div className="container-clickup-3_0_body">
+          <div className="container-clickup-3_0">
+            <div className="container-clickup-3_0_icone">
+              <img src={clickup_v3} alt="clickup_v3" />
+            </div>
+            <div className="container-clickup-3_0_texte1">
+              <h1>Productivity, reimagined.</h1>
+            </div>
+
+            <div className="container-clickup-3_0_texte2">
+              <p>The next evolution of team productivity and collaboration is here. Unlock the<br></br>next generation of productivity with the infinite possibilities of ClickUp 3.0.</p>
+            </div>
+
+          </div>
+
+          <div className="container-clickup-3_0_icone2">
+            <img src={CU_3_0_Teaser } alt="U_3_0_Teaser" />
+          </div>
+
+          <div className="container-clickup-3_0_texto3">
+            <div container-clickup-3_0_texto_3_1>
+              <p>Discover What's comming</p>
+            </div>
+
+            <div className="container-clickup-3_0_texto_3_icone" ></div>
+
+          </div>
+
         </div>
 
       </div>
